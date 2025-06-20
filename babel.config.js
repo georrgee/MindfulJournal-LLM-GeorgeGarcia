8882b642c4b28803d442ceb546a519b1dcb999c3
George Garcia <garcia.george.b@gmail.com>
2025-06-20 13:07:04 -0700

    installed frameworks

---
+++ babel.config.js
@@ -0,0 +1,23 @@
+module.exports = function (api) {
+  api.cache(true);
+  return {
+    presets: ["babel-preset-expo"],
+    plugins: [
+      "react-native-reanimated/plugin",
+      [
+        "module:react-native-dotenv",
+        {
+          moduleName: "@env",
+          path: ".env",
+        },
+      ],
+      [
+        "@tamagui/babel-plugin",
+        {
+          components: ["@tamagui/core"],
+          config: "./tamagui.config.ts",
+        },
+      ],
+    ],
+  };
+};
