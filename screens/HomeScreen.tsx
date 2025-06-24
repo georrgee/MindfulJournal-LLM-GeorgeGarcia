10fcf366fff2b74dc1b6b43b22bd59100f7b870f
George Garcia <garcia.george.b@gmail.com>
2025-06-23 17:30:22 -0700

    Screen + Components folder

---
+++ screens/HomeScreen.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+// import { styles } from './styles';
+
+/**
+  * @author George Garcia
+  * @description **()** - 
+  */
+
+const HomeScreen: React.FC = (props) => {
+
+  return (
+    <View>
+      <Text>This is the HomeScreen</Text>
+    </View>
+  );
+
+};
+
+export default HomeScreen;
+
+/**
+  * @author George Garcia
+  * @description **(Styling)** - 
+  */
+export const styles = StyleSheet.create({
+
+});
